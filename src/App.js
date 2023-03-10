17be0b6692f5718d6d2ab20dc6f1a618dbb31289
Michael Blackmer <xmcbii@gmail.com>
2023-03-10 15:35:26 -0600

    multiple components added / cart modal functioning

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { Fragment, useState } from 'react';
+import Meals from './components/Meals/Meals';
+import Header from './components/Layout/Header';
+import Cart from './components/Cart/Cart';
+
 function App() {
+  const [cartView, setCartView] = useState(false);
+
+  const showCartHandler = () => {
+    setCartView(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartView(false);
+  }
+
+
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <Fragment>
+      {cartView && <Cart onClose={ hideCartHandler } />}
+      <Header onShowCart={showCartHandler}  />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
   );
 }
 
