01d7ffa4d27ae39fb1647fe3fbd252be7568c88a
Michael Blackmer <xmcbii@gmail.com>
2023-03-10 16:14:43 -0600

    Context and provider created to handle Cart actions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { Fragment, useState } from 'react';
+import { useState } from 'react';
 import Meals from './components/Meals/Meals';
 import Header from './components/Layout/Header';
 import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
   const [cartView, setCartView] = useState(false);
@@ -16,13 +17,13 @@ function App() {
 
 
   return (
-    <Fragment>
+    <CartProvider>
       {cartView && <Cart onClose={ hideCartHandler } />}
       <Header onShowCart={showCartHandler}  />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
